b0673771e69be097d42bb929814a10f3a9d679f2
Ghad <zimobie@gmail.com>
2020-05-06 22:19:13 +0100

    created a card for overview stats

--- social-media-dashboard-react/src/components/Followers.jsx
+++ social-media-dashboard-react/src/components/Followers.jsx
@@ -1,32 +1,30 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 const Followers = () => {
   return (
-    <Fragment>
+    <div>
       <div>
-        <div>
-          <div>@nathanf</div>
-          <div>1987 followers</div>
-          <div>12 Today</div>
-        </div>
-        <div>
-          <div>@nathanf</div>
-          <div>1044 followers</div>
-          <div>99 Today</div>
-        </div>
-        <div>
-          <div>@realnathanf</div>
-          <div>11k followers</div>
-          <div>1099 Today</div>
-        </div>
-        <div>
-          <div>@Nathan F.</div>
-          <div>8239 subscribers</div>
-          <div>144 Today</div>
-        </div>
+        <div>@nathanf</div>
+        <div>1987 followers</div>
+        <div>12 Today</div>
       </div>
-    </Fragment>
-  )
-}
+      <div>
+        <div>@nathanf</div>
+        <div>1044 followers</div>
+        <div>99 Today</div>
+      </div>
+      <div>
+        <div>@realnathanf</div>
+        <div>11k followers</div>
+        <div>1099 Today</div>
+      </div>
+      <div>
+        <div>@Nathan F.</div>
+        <div>8239 subscribers</div>
+        <div>144 Today</div>
+      </div>
+    </div>
+  );
+};
 
-export default Followers
+export default Followers;
---
+++ social-media-dashboard-react/src/components/OverviewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OverviewCard = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default OverviewCard;
