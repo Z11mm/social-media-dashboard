100f9fede95866ed6898d0b91e2c2de8e7f8b8dc
Z11mm <zimobiechina@gmail.com>
2020-05-09 20:38:36 +0100

    adding elements to OverviewCard component

--- social-media-dashboard-react/src/App.js
+++ social-media-dashboard-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Followers from './components/FollowersCard'
 import './App.css';
+import OverviewCard from './components/OverviewCard';
 
 const App = () => {
   return (
     <div>
       <Followers />
+      <OverviewCard />
     </div>
   );
 }
--- social-media-dashboard-react/src/components/OverviewCard.jsx
+++ social-media-dashboard-react/src/components/OverviewCard.jsx
@@ -3,7 +3,54 @@ import React from 'react';
 const OverviewCard = () => {
   return (
     <section>
-      <div></div>
+      <div>
+        <div>Page Views</div>
+        <div>image</div>
+        <div>87</div>
+        <div>30%</div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </section>
   );
 }
