38775dafbc939b0c7a14e8d26d3e568b1689fb9c
Z11mm <zimobiechina@gmail.com>
2020-05-12 21:57:15 +0100

    Add icon
    Include icons in OverviewCard and moved images directory into src

--- social-media-dashboard-react/src/components/OverviewCard/OverviewCard.jsx
+++ social-media-dashboard-react/src/components/OverviewCard/OverviewCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const OverviewCard = ({ type, count, currentStat}) => {
+const OverviewCard = ({ type, count, currentStat, icon, rateIcon }) => {
   return (
     <div>
       <div>{type}</div>
-      <div>image</div>
+      <div><img src={icon} alt=""/></div>
       <div>{count}</div>
-      <div>{currentStat}</div>
+      <div><img src={rateIcon} alt=""/>{currentStat}</div>
     </div>
   );
-}
+};
 
 export default OverviewCard;
---
+++ social-media-dashboard-react/src/components/icons.js
@@ -0,0 +1,15 @@
+import twitter from '../images/icon-twitter.svg'
+import facebook from '../images/icon-facebook.svg'
+import instagram from '../images/icon-instagram.svg'
+import youtube from '../images/icon-youtube.svg'
+import up from '../images/icon-up.svg'
+import down from '../images/icon-down.svg'
+
+export default {
+  twitterIcon: twitter,
+  facebookIcon: facebook,
+  instagramIcon: instagram,
+  youtubeIcon: youtube,
+  riseIcon: up,
+  fallIcon: down
+};
