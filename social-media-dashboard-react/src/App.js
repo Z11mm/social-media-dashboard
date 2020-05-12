963386712d03effc50a5dceeaf0907e55f86e4d3
Z11mm <zimobiechina@gmail.com>
2020-05-12 08:17:34 +0100

    components folder structure
    Rearranged components folder structure to use the container pattern

--- social-media-dashboard-react/src/App.js
+++ social-media-dashboard-react/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Followers from './components/FollowersCard'
+import Followers from './components/FollowersCard/FollowersCard'
 import './App.css';
-import OverviewCard from './components/OverviewCard';
+import OverviewCard from './components/OverviewCard/OverviewCard';
 
 const App = () => {
   return (
