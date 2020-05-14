e84237235966e584ce7f55945c1aa1d3301466f7
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-05-14 22:25:20 +0100

    Add header component to App
    Add header directory to components directory and add Header to App

--- social-media-dashboard-react/src/App.js
+++ social-media-dashboard-react/src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import FollowersCardContainer from './components/FollowersCard/FollowersCardContainer';
 import './App.css';
 import OverviewCardContainer from './components/OverviewCard/OverviewCardContainer';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <FollowersCardContainer />
       <OverviewCardContainer />
     </div>
