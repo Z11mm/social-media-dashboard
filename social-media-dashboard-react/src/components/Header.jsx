0d941d41433d8cd1247ec04e2a2f2d935801c534
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-05-14 22:20:48 +0100

    Add header

--- social-media-dashboard-react/src/components/Header.jsx
+++ social-media-dashboard-react/src/components/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <div></div>
+    <div>
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers: 23,004</p>
+    </div>
+    <div>
+      <span>Dark Mode</span>
+        <label for="toggle-switch" class="toggle-switch" aria-label="toggle-switch">
+          <input id="toggle-switch" type="checkbox" />
+          <span class="slider round"></span>
+        </label> 
+    </div>
   </header>
 );
 
