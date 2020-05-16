ba6f83ecb20485648de08021c567e8bbb329b647
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-05-16 17:31:12 +0100

    Delete unused style file

--- social-media-dashboard-react/src/App.js
+++ social-media-dashboard-react/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import FollowersCardContainer from './components/FollowersCard/FollowersCardContainer';
-import './App.css';
 import OverviewCardContainer from './components/OverviewCard/OverviewCardContainer';
 import Header from './components/header/Header';
 import { GlobalStyle } from './GlobalStyles';
