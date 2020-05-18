de2d3e0eb1f7999fe62a3e88d60b87b522866f10
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-05-18 21:33:48 +0100

    Style: Add header font styles

--- social-media-dashboard-react/src/components/header/Header.jsx
+++ social-media-dashboard-react/src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Toggle from '../toggle/Toggle';
-import { HeaderBlock, Heading } from './HeaderStyles';
+import { HeaderBlock, Heading, Total } from './HeaderStyles';
 
 const Header = () => (
   <HeaderBlock>
     <Heading>
       <h2>Social Media Dashboard</h2>
-      <p>Total Followers: 23,004</p>
+      <Total>Total Followers: 23,004</Total>
     </Heading>
     <Toggle />
   </HeaderBlock>
--- social-media-dashboard-react/src/components/header/HeaderStyles.js
+++ social-media-dashboard-react/src/components/header/HeaderStyles.js
@@ -6,5 +6,13 @@ export const HeaderBlock = styled.header`
 `;
 
 export const Heading = styled.div`
-  border-bottom: 1px solid #000;
+  border-bottom: 1px solid hsl(228, 12%, 44%);
+  line-height: 1.5;
+  padding-bottom: 0.5rem;
+`;
+
+export const Total = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  color: hsl(228, 12%, 44%);
 `;
