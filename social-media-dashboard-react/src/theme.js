a3f3992520c395bd55b3e5c456237316b671710e
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-05-19 15:02:22 +0100

    Style: Add toggle theme styles

--- social-media-dashboard-react/src/theme.js
+++ social-media-dashboard-react/src/theme.js
@@ -6,17 +6,20 @@ const veryDarkBlue = 'hsl(230, 17%, 14%)';
 const veryDarkBlueTop = 'hsl(232, 19%, 15%)';
 const darkDesaturatedBlue = 'hsl(228, 28%, 20%)';
 const desaturatedBlue = 'hsl(228, 34%, 66%)';
+const toggleBackground = 'hsl(230, 22%, 74%)'
 
 export const lightTheme = {
-  body: `linear-gradient(${ veryPaleBlue } 0 %, ${ veryPaleBlue } 10 %, ${ white } 10 %, ${white} 100%)`,
+  body: `linear-gradient(${ veryPaleBlue } 0%, ${ veryPaleBlue } 10%, ${ white } 10%, ${white} 100%)`,
   cardBackground: ${ lightGreyishBlue },
   textDarkGreyBlue: ${ darkGreyishBlue },
-  textVeryDarkBlue: ${veryDarkBlue}
+  textVeryDarkBlue: ${ veryDarkBlue },
+  toggleBackground: ${toggleBackground}
 };
   
 export const darkTheme = {
   body: `linear-gradient(${ veryDarkBlueTop } 0 %, ${ veryDarkBlueTop } 10 %, ${ veryDarkBlue } 10 %, ${veryDarkBlue} 100%)`,
   cardBackground: ${ darkDesaturatedBlue },
   textDesaturatedBlue: ${ desaturatedBlue },
-  textWhite: ${white}
+  textWhite: ${ white },
+  toggleBackground: 'linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))'
 }
